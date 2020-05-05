0a9ef1613111bdccfb6e7476f0c55d46146a6a25
Tevin <tevinward7@gmail.com>
2020-05-05 17:20:38 -0400

    made routes work

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const path = require("path");
 
 
 const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+app.get("/notes", (require, response) => {
+    response.sendFile(path.join(__dirname, "../../notes.html"));
+});
+
+app.get("*", (require, response) => {
+    response.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT http://localhost:${PORT}`);
+});
