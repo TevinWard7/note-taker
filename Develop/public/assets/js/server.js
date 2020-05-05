94a610240d7d12ac186464ae6d96ab04e90f9659
Tevin <tevinward7@gmail.com>
2020-05-05 18:02:58 -0400

    made api/notes route work

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require('fs');
 
 
 const app = express();
@@ -8,17 +9,28 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Index route
+app.get("/", (require, response) => {
+    response.sendFile(path.join(__dirname, "../../index.html"));
+});
 
-
-
-
+// Notes route
 app.get("/notes", (require, response) => {
     response.sendFile(path.join(__dirname, "../../notes.html"));
 });
 
-app.get("*", (require, response) => {
-    response.sendFile(path.join(__dirname, "../../index.html"));
+// JSon Notes route
+app.get("/api/notes", (require, response) => {
+    fs.readFile("db.json", "utf-8", (err, data) => {
+        console.log(data)
+        return response.json(data)
+    })
+
+
 });
+
+
+
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT http://localhost:${PORT}`);
