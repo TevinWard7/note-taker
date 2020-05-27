7df651f8dfcd1d530f536e9ee045e5e5032250c0
Tevin <tevinward7@gmail.com>
2020-05-26 21:38:53 -0400

    made /api/notes route work properly

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -19,12 +19,13 @@ app.get("/notes", (request, response) => {
     response.sendFile(path.join(__dirname, "../../notes.html"));
 });
 
-// JSon Notes route
+// Json Notes route
 app.get("/api/notes", (request, response) => {
 
-    fs.readFile("db.json", "utf-8", (err, data) => {
-        console.log(data)
-        return response.json(data)
+    fs.readFile("db.json", (err, data) => {
+        let parsedData = JSON.parse(data);
+        console.log(parsedData)
+        return response.json(parsedData)
     })
 });
 
@@ -32,7 +33,10 @@ app.post("/api/notes", (request, response) => {
 
     const newNote = JSON.stringify(request.body);
 
-    fs.writeFile('db.json', newNote, () => { });
+    fs.appendFile('db.json', newNote, () => {
+
+    });
+    response.redirect("/");
 });
 
 app.delete("/api/notes/:id", (request, response) => {
